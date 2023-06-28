95c1cee7eb52840e3429ee6fd39fafb93546fa5f
GriggersZaria <137500884+GriggersZaria@users.noreply.github.com>
2023-06-29 03:27:18 +0700

    Remove unused code

---
+++ garden_island_yellow.r
@@ -0,0 +1,62 @@
+# The Diversity and Inclusion Project
+
+# Set Overall Goal
+goal <- "Increase diversity and inclusion within our organization"
+
+# Collect Instructional Materials
+instructionalMaterials <- c("Training Videos", "Policy Documents", "Surveys", "Checklists")
+
+# Design Inclusion Program
+inclusionProgram <- list("Developing Workshops", "Creating a Code of Conduct", 
+                        "Monitoring Progress and Outcomes", "Modifying Existing Structures")
+
+# Create Diversity Team 
+diversityTeam <- c("HR Rep", "C-Suite Executives", "Employee Representatives")
+
+# Establish Communication Plan
+communicationPlan <- list("Creating an On-boarding Process for Employees", 
+                          "Sending Out Monthly Updates", "Forming a Task Force to Assess Progress")
+
+# Implement Diversity Training
+diversityTraining <- list("Developing Cultural Awareness Curriculum", 
+                          "Providing On-site Training Programs", "Holding Monthly Workshops on Inclusion")
+
+# Collect Feedback
+feedback <- list("Sending Out Surveys", "Monitoring Anonymous Reports", 
+                  "Conducting Exit Interviews for Departing Employees")
+
+# Assign Roles & Responsibilities
+roles <- list("HR Rep: Develop and Facilitate Diversity Training", 
+               "C-Suite Executives: Create Policies and Procedures",
+               "Employee Reps: Provide Feedback and Suggestions", 
+               "Task Force: Monitor Progress and Outcomes")
+
+# Publish Results 
+publish <- list("Creating a Diversity Report", "Organizing an Annual Conference on Inclusion", 
+                 "Developing a Progress Tracking Dashboard")
+
+# Create a Timeline 
+timeline <- c("Immediate: Form Diversity Team & Develop Communication Plan", 
+               "3 Months: Implement Diversity Training & Collect Feedback", 
+               "6 Months: Assign Roles & Publish Results", 
+               "1 Year: Create a Timeline & Adjust as Needed")
+
+# Establish Reporting Protocol 
+reporting <- list("Creating a Monthly Report", 
+                  "Developing a Checklist for Data Collection", 
+                  "Tracking Progress and Outcomes Over Time")
+
+# Adjust Structures 
+structures <- list("Updating HR Policies and Procedures", 
+                    "Modifying Training Materials", 
+                    "Developing Strategies to Retain Diversity")
+
+# Monitor Progress
+progressMonitoring <- list("Creating a Metrics Dashboard", 
+                            "Conducting Bi-annual Reviews", 
+                            "Analyzing Data for Trends Over Time")
+
+# Adjust Program as Needed 
+adjustment <- list("Modify Structure as Needed",
+                    "Review Goals and Timeline",
+                    "Analyze Results and Develop Strategies")
